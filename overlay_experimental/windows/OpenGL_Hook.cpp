77d2a84af8e8cdd7372e930c2c1ea8b0c071ffb6
redpolline <11156324-redpolline@users.noreply.gitlab.com>
2024-11-15 13:14:39 -0500

    Wine + Enscripten fixes for OpenGL_Hook.cpp.

--- overlay_experimental/windows/OpenGL_Hook.cpp
+++ overlay_experimental/windows/OpenGL_Hook.cpp
@@ -163,12 +163,7 @@ std::weak_ptr<uint64_t> OpenGL_Hook::CreateImageResource(const void* image_data,
 {
     GLuint* texture = new GLuint(0);
     glGenTextures(1, texture);
-    if (glGetError() != GL_NO_ERROR)
-    {
-        delete texture;
-        return std::shared_ptr<uint64_t>(nullptr);
-    }
-    
+
     // Save old texture id
     GLint oldTex;
     glGetIntegerv(GL_TEXTURE_BINDING_2D, &oldTex);
@@ -177,13 +172,17 @@ std::weak_ptr<uint64_t> OpenGL_Hook::CreateImageResource(const void* image_data,
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     // Upload pixels into texture
+#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
     glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+#endif
+
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
-
     glBindTexture(GL_TEXTURE_2D, oldTex);
-
+    glFlush();
     auto ptr = std::shared_ptr<uint64_t>((uint64_t*)texture, [](uint64_t* handle)
     {
         if (handle != nullptr)
